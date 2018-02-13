c19ffbb5cf3789fb58bf44c926298b557915e3fe
Megan Poosangtongchai <mpoosangtongchai@gmail.com>
2018-02-13 06:58:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function prependKitten(name) {
 }
 
 function removeLastKitten(name) {
-  return kittens.slice(0 -)
+  return kittens.slice(0, kittens.length - 1);
 }
 
 function removeFirstKitten(name) {
